3a2bdaabcb032862e0f23624afad59b0144b8ff9
Matt Conrad <mmconrad@gmail.com>
2017-07-04 07:31:12 +0200

    Update plaid link success handling; Add return button

--- fe-web/src/app/containers/PlaidLinkApp/index.tsx
+++ fe-web/src/app/containers/PlaidLinkApp/index.tsx
@@ -22,23 +22,40 @@ export class PlaidLinkApp extends React.Component<PlaidLinkAppProps, PlaidLinkAp
   }
 
   handleOnSuccess(token: string, metadata) {
+    const plaidLinkStore = this.props[STORE_PLAID_LINK] as PlaidLinkStore;
+    plaidLinkStore.setPublicToken(token);
+
     // TODO: FIRST: send user back to Unity Scene with public token data.
-    this.props[STORE_PLAID_LINK].setPublicToken(token);
+
     // TODO: LATER: send token to client server to be saved.
+  }
+
+  returnToUnity() {
+
   }
 
   render() {
     const plaidLinkStore = this.props[STORE_PLAID_LINK] as PlaidLinkStore;
 
     return (
-      <PlaidLink
-        publicKey={plaidLinkStore.publicKey}
-        product="auth"
-        env={plaidLinkStore.env}
-        clientName="card-pop"
-        onSuccess={this.handleOnSuccess}
-        apiVersion="v2"
-        />
+      <div className="button-container">
+        <PlaidLink
+          publicKey={plaidLinkStore.publicKey}
+          product="auth"
+          env={plaidLinkStore.env}
+          clientName="card-pop"
+          onSuccess={this.handleOnSuccess}
+          apiVersion="v2"
+          buttonText="Add An Account"
+          className="card-pop-button"
+          />
+        <button
+          className="card-pop-button"
+          onClick={this.returnToUnity}
+        >
+          <span>Return to Video Feed</span>
+        </button>
+      </div>
     );
   }
 };
