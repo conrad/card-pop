0022f991bbdf379b02107bd71a16bd79fb57fecb
Matt Conrad <mmconrad@gmail.com>
2017-07-14 09:43:22 +0200

    Add BrowserListener for logging and reacting to messages from browser

---
+++ unity-card-visualizer/Assets/BrowserListener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrowserListener : MonoBehaviour {
+
+	public string javaScriptCode = "alert('pong!')";
+
+	void Start() {
+		InAppBrowserBridge bridge = FindObjectOfType<InAppBrowserBridge>();
+		bridge.onJSCallback.AddListener(OnMessageFromJS);
+	}
+
+	void OnMessageFromJS(string jsMessage) {
+		Debug.Log(jsMessage);
+
+		if (jsMessage.Equals("close-browser")) {
+			InAppBrowser.CloseBrowser();
+		}
+	}
+}
